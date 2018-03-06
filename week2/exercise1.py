b7a9d29f9556711165e421a41825a138efbd3bb4
Nelson Chen <nelson.chen1999@gmail.com>
2018-03-06 16:35:58 +1100

    Committing before battery dies
    there are no power ports in this room

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -10,16 +10,16 @@ import platform
 
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
-
+# I think this will associate the list "['what', 'does', 'this', 'line', 'do', '?']" with the variable "some_words"
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
-
-for word in some_words:
+# I think this will print the words that are in the list in the variable "some_words"
+for word in some_words: #This line printed each each word in the list "some_words" on their own lines
     print(word)
 
 for x in some_words:
     print(x)
-
-print(some_words)
+# I think this will just print "['what', 'does', 'this', 'line', 'do', '?']" as it is
+print(some_words) #It printed "['what', 'does', 'this', 'line', 'do', '?']" as it was
 
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,7 +7,7 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    pass
+    
 
 
 def fix_it(moves=True, should_move=True):
