2fefa0da1eeec5d8bf91623c9a17f257eeb3b7a9
Nelson Chen <nelson.chen1999@gmail.com>
2018-04-09 23:08:09 +1000

    Tried some of week5 exercise1

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -49,7 +49,13 @@ def do_bunch_of_bad_things():
 # return a list of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
-    pass
+    countdown_a = []
+    counter = start
+    while counter < stop:
+        countdown_a.append(message + counter)
+        counter += 1
+    print(countdown_a)
+    print(completion_message)
 
 
 # TRIANGLES
