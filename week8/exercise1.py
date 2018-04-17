565a78d2fec81192492b4497c337db420273b808
Nelson Chen <nelson.chen1999@gmail.com>
2018-04-17 16:51:29 +1000

    Test

--- week8/exercise1.py
+++ week8/exercise1.py
@@ -17,7 +17,7 @@ def greet(name="Towering Timmy"):
     return a string of "Hello" and the name argument.
     E.g. if given as "Towering Timmy" it should return "Hello Towering Timmy"
     """
-    pass
+    return "Hello " + name
 
 
 def three_counter(input_list=[1, 4, 3, 5, 7, 1, 3, 2, 3, 3, 5, 3, 7]):
@@ -26,7 +26,7 @@ def three_counter(input_list=[1, 4, 3, 5, 7, 1, 3, 2, 3, 3, 5, 3, 7]):
     Return an integer.
     TIP: the test will use a different input_list, so don't just return 5
     """
-    pass
+    return input_list.count(3)
 
 
 def fizz_buzz():
@@ -44,7 +44,17 @@ def fizz_buzz():
     if it is. E.g. [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, ...]
     """
     fizzBuzzList = []
-    # your code here
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5==0:
+            fizzBuzzList.append("FizzBuzz")
+            continue
+        elif i % 3 == 0:
+            fizzBuzzList.append("Fizz")
+            continue
+        elif i % 5 == 0:
+            fizzBuzzList.append("Buzz")
+        else:
+            fizzBuzzList.append(i)
     return fizzBuzzList
 
 
@@ -57,7 +67,7 @@ def put_behind_bars(input_string="very naughty boy"):
     TIP: conside using the 'join' method in Python.
     TIP: make sure that you have a pipe on both ends of the string.
     """
-    pass
+    return "|" + "|".join(input_string) +"|"
 
 
 def pet_filter(letter="a"):
@@ -70,7 +80,8 @@ def pet_filter(letter="a"):
             "bali cattle", "gayal", "turkey", "goldfish", "rabbit", "koi",
             "canary", "society finch", "fancy mouse", "siamese fighting fish",
             "fancy rat and lab rat", "mink", "red fox", "hedgehog", "guppy"]
-    pass
+    filterletter = [i for i in pets if letter in i]
+    return filterletter
 
 
 def best_letter_for_pets():
@@ -81,7 +92,6 @@ def best_letter_for_pets():
     """
     import string
     the_alphabet = string.ascii_lowercase
-    pass
 
 
 def make_filler_text_dictionary():
@@ -113,7 +123,7 @@ def make_filler_text_dictionary():
     """
     
     import requests
-    return
+    return 
 
 
 def random_filler_text(number_of_words=200):
